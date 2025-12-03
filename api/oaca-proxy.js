e8e67b120d01363e0e5d5fefce6ebdc4a994e31e
MoezAyadiDev <moez.ayadi.dev@gmail.com>
2025-12-03 19:42:39 +0100

    delete all imports

--- api/oaca-proxy.js
+++ api/oaca-proxy.js
@@ -1,5 +1,5 @@
 // import https from "https";
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
